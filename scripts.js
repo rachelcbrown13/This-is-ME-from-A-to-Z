a20bc475e944553d1b7683faadc62b2a2c3d9ff5
Rachel Brown <rachelcbrown13@gmail.com>
2020-10-06 06:17:30 -0500

    test

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ $(function(){
             "./img/letter_D.png", "./img/letter_E.png", "./img/letter_F.png", "img/letter_G.png", 
             "img/letter_H.png", "img/letter_I.png", "img/letter_J.png", "img/letter_K.png", 
             "img/letter_L.png", "img/letter_M.png", "img/letter_N.png", "img/letter_O.png", 
-            "img/letter_percy.png", "img/letter_q.png", "img/letter_r.png", "img/letter_s.png");
+            "img/letter_Percy.png", "img/letter_Q.png", "img/letter_R.png", "img/letter_S.png");
         var randomNum = Math.floor(Math.random() * myPix.length);
         img[i].src = myPix[randomNum];
       }
