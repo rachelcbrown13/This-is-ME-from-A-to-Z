af66ed8f09218578de7842f44d5b35b84162a6ac
Rachel Brown <rachelcbrown13@gmail.com>
2020-10-06 06:13:00 -0500

    image changes

--- scripts.js
+++ scripts.js
@@ -3,10 +3,10 @@ $(function(){
   function choosePic() {
     var img = document.querySelectorAll(".circle");
        for (i=0; i < img.length; i++){
-        const myPix = new Array("./img/letter_A.png", "./img/Letter_b.png", "./img/letter_c.png", 
-            "./img/letter_d.png", "./img/letter_e.png", "./img/letter_f.png", "img/letter_g.png", 
-            "img/letter_h.png", "img/letter_i.png", "img/letter_j.png", "img/letter_k.png", 
-            "img/letter_l.png", "img/letter_m.png", "img/letter_n.png", "img/letter_o.png", 
+        const myPix = new Array("./img/letter_A.png", "./img/Letter_B.png", "./img/letter_C.png", 
+            "./img/letter_D.png", "./img/letter_E.png", "./img/letter_F.png", "img/letter_G.png", 
+            "img/letter_H.png", "img/letter_I.png", "img/letter_J.png", "img/letter_K.png", 
+            "img/letter_L.png", "img/letter_M.png", "img/letter_N.png", "img/letter_O.png", 
             "img/letter_percy.png", "img/letter_q.png", "img/letter_r.png", "img/letter_s.png");
         var randomNum = Math.floor(Math.random() * myPix.length);
         img[i].src = myPix[randomNum];
