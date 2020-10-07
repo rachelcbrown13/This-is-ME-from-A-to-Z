bda61f6c056cd8d4364487db350472b681239bec
Rachel Brown <rachelcbrown13@gmail.com>
2020-10-06 20:53:32 -0500

    testing

--- scripts.js
+++ scripts.js
@@ -3,8 +3,8 @@ $(function(){
   function choosePic() {
     var img = document.querySelectorAll(".circle");
        for (i=0; i < img.length; i++){
-        const myPix = new Array("./img/letter_A.png", "./img/Letter_B.png", "./img/letter_C.png", 
-            "./img/letter_D.png", "./img/letter_E.png", "./img/letter_F.png", "img/letter_G.png", 
+        const myPix = new Array("img/letter_A.png", "img/Letter_B.png", "img/letter_C.png", 
+            "img/letter_D.png", "img/letter_E.png", "img/letter_F.png", "img/letter_G.png", 
             "img/letter_H.png", "img/letter_I.png", "img/letter_J.png", "img/letter_K.png", 
             "img/letter_L.png", "img/letter_M.png", "img/letter_N.png", "img/letter_O.png", 
             "img/letter_Percy.png", "img/letter_Q.png", "img/letter_R.png", "img/letter_S.png");
