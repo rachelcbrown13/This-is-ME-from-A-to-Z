aad9a749e17f628930a65bb51527e355be53394d
Rachel Brown <rachelcbrown13@gmail.com>
2020-10-06 21:36:29 -0500

    THIS IS IT!

--- scripts.js
+++ scripts.js
@@ -3,11 +3,11 @@ $(function(){
   function choosePic() {
     var img = document.querySelectorAll(".circle");
        for (i=0; i < img.length; i++){
-        const myPix = new Array("img/letter_A.png", "img/Letter_B.png", "img/letter_C.png", 
-            "img/letter_D.png", "img/letter_E.png", "img/letter_F.png", "img/letter_G.png", 
-            "img/letter_H.png", "img/letter_I.png", "img/letter_J.png", "img/letter_K.png", 
-            "img/letter_L.png", "img/letter_M.png", "img/letter_N.png", "img/letter_O.png", 
-            "img/letter_Percy.png", "img/letter_Q.png", "img/letter_R.png", "img/letter_S.png");
+        const myPix = new Array("img/Letter_A.png", "img/Letter_B.png", "img/Letter_C.png", 
+            "img/Letter_D.png", "img/Letter_E.png", "img/Letter_F.png", "img/Letter_G.png", 
+            "img/Letter_H.png", "img/Letter_I.png", "img/Letter_J.png", "img/Letter_K.png", 
+            "img/Letter_L.png", "img/Letter_M.png", "img/Letter_N.png", "img/Letter_O.png", 
+            "img/Letter_Percy.png", "img/Letter_Q.png", "img/Letter_R.png", "img/Letter_S.png");
         var randomNum = Math.floor(Math.random() * myPix.length);
         img[i].src = myPix[randomNum];
       }
