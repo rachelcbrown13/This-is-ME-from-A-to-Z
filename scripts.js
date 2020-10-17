bfd1fef0ef855caca5f3f514466ef011c4c21a1a
Rachel Brown <rachelcbrown13@gmail.com>
2020-10-17 10:44:39 -0500

    update Oct 16th

--- scripts.js
+++ scripts.js
@@ -7,17 +7,13 @@ $(function(){
             "img/Letter_D.png", "img/Letter_E.png", "img/Letter_F.png", "img/Letter_G.png", 
             "img/Letter_H.png", "img/Letter_I.png", "img/Letter_J.png", "img/Letter_K.png", 
             "img/Letter_L.png", "img/Letter_M.png", "img/Letter_N.png", "img/Letter_O.png", 
-            "img/Letter_Percy.png", "img/Letter_Q.png", "img/Letter_R.png", "img/Letter_S.png");
+            "img/Letter_Percy.png", "img/Letter_Perry.png", "img/Letter_Q.png", "img/Letter_R.png", 
+            "img/Letter_S.png", "img/Letter_Talia.png", "img/Letter_Tyler.png", "img/Letter_Taylor.png", 
+            "img/Letter_U.png", "img/Letter_V.png", "img/Letter_W.png", "img/Letter_X.png",
+            "img/Letter_Y.png", "img/Letter_Z.png");
         var randomNum = Math.floor(Math.random() * myPix.length);
         img[i].src = myPix[randomNum];
       }
-  }
-  $(".cover").fadeIn(10000);
-
-  function total(){
-     var quantity = 0;
-     $("bookNum").val = el ;
-     console.log ("this");
   }
 });
 
